f8dc647164a553096bfd74971a1e1470598db01b
Evan Farnsworth <farnsworth.evan@yahoo.com>
2023-05-16 00:14:50 -0700

    optimized storage of item prices

--- craft_analyzer.py
+++ craft_analyzer.py
@@ -5,8 +5,10 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-windows_dir_path = r'C:\python_practice\python_stuff\tarkov_api_test'
+windows_dir_path = r'C:\python_practice\python_stuff\tarkov_api_test\crafts'
 #mac_dir_path = r''
+# This code is kind of redundant now. A better price storage system has been created. 
+# Will keep this running till end of wipe.
 
 def craft_query():
     return """
@@ -15,6 +17,7 @@ def craft_query():
             rewardItems{
                 item{
                     name
+                    id
                 }
                 count
             }
@@ -36,8 +39,10 @@ def craft_query():
         """ 
     
 def find_craft(crafted_item):
-    cost_dict = {'Crafted item': crafted_item, 'Required items': {}} 
+    cost_dict = {'Crafted item': crafted_item, 'ID': '', 'Required items': {}} 
     reward_count = 0
+    id = ''
+    total_cost = 0
 
     response = requests.post('https://api.tarkov.dev/graphql', json={'query': craft_query()})
     if response.status_code != 200:
@@ -49,6 +54,7 @@ def find_craft(crafted_item):
         for reward_item in craft['rewardItems']:
             if reward_item['item']['name'] == crafted_item:
                 reward_count = reward_item['count']
+                id = reward_item['item']['id']
                 for required_items in craft['requiredItems']:
                     name = required_items['item']['name']
                     if len(required_items['attributes']) > 0:
@@ -63,13 +69,13 @@ def find_craft(crafted_item):
                         cost = cheapest_total
                     cost_dict['Required items'][name] = cost
                     
-    total_cost = 0   
             
     for name, cost in cost_dict['Required items'].items():
         total_cost += cost
             
     current_date = datetime.now().strftime('%Y-%m-%d')
     
+    cost_dict['ID'] = id
     cost_dict['Price (RUB)'] = total_cost
     cost_dict['Items crafted'] = reward_count
     cost_dict['Price per item'] = round((total_cost / reward_count), 2)
@@ -86,45 +92,34 @@ def save_craft_json(crafted_item):
     
     if os.path.exists(file_path):
         print('file found')
-        with open(file_path, 'a') as file:
-            json.dump(cost_dict, file, indent=4)
-            file.write(',\n') 
+        with open(file_path, 'r') as file:
+            existing_data = json.load(file)
+            
     else:
-        print('file not found: created new file')
-        with open(file_path, 'w') as file:
-            json.dump(cost_dict, file, indent=4)
-            file.write(',\n')
+        print('file not found: creating new file')
+        existing_data = []
+        
+    existing_data.append(cost_dict)
     
-# TODO: create function to graph json file to several graphs:
-# line chart for craft cost, line chart showing cost of all required items)   
-    
+    with open(file_path, 'w') as file:
+        json.dump(existing_data, file, indent=4)
+        
 def graph_costs(crafted_item):
     file_path = windows_dir_path + '\\' + crafted_item +  '_craft_data.json'
     with open(file_path, 'r') as file:
-        craft_data = json.load(file)
-    
-    
-    df = pd.json_normalize(craft_data)
-    
+        df = pd.read_json(file)
     plt.plot(df['Date'], df['Price (RUB)'])
     plt.show()
     
             
 # common crafted items:
-    # 7.62x39mm BP gzh
-    # 9x21mm BT gzh
-    # 5.56x45mm M995
-    # 9x19mm RIP
-    # Ox bleach
-    # Soap
-    # Fleece fabric
-    # Propital regenerative stimulant injector
-    # Pile of meds
-    # AI-2 medkit
-
-    
-print(find_craft('9x21mm BT gzh'))
-
-save_craft_json('7.62x39mm BP gzh')
-
-#graph_costs('7.62x39mm BP gzh')
+#     7.62x39mm BP gzh
+#     9x21mm BT gzh
+#     5.56x45mm M995
+#     9x19mm RIP
+#     Ox bleach
+#     Soap
+#     Fleece fabric
+#     Propital regenerative stimulant injector
+#     Pile of meds
+#     AI-2 medkit
